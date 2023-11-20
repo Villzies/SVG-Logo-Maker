5d4ca8eee5e83107c3ede9144ea008e795b681f8
Villzies <gemberling.stephan@att.net>
2023-11-19 21:39:14 -0600

    Added comments

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//Dependencies
 const inquirer = require('inquirer');
 const questions = require('./lib/questions.js');
 const fileName = "./examples/logo.svg";
 const fs = require('fs');
 const setShape = require('./lib/setShape.js')
 
+//Creates the logo.svg file
 function createLogo(response) {
     const svg = setShape(response);
     fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
 }
 
+//Code for initializing and running inquirer
 function init() {
     inquirer 
     .prompt(questions)
