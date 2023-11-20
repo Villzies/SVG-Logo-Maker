2b6c32ae50e95d5f2d312e5f9f0cd4e38f1fcd5b
Villzies <gemberling.stephan@att.net>
2023-11-19 20:34:52 -0600

    Added code for each shape and added a sample test

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const questions = require('./lib/questions.js');
+const fileName = "./examples/logo.svg";
+const setShape = require('./lib/setShape.js')
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+}
+
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
